4bdee13ba55db4814d80db028532b107ee4bc009
Jean Cabral <jeancabral@gmail.com>
2020-04-15 16:00:07 -0300

    Estado e Imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 
 import Header from './components/Header'
 
 function App(){
+
+  const [projects, setProjects] = useState(['Desenvolvimento de App', 'Front-end web', 'Mobile'])
+
+  function handleAddProject() {
+    //projects.push(`Novo Projeto ${Date.now()}`)
+
+    setProjects([...projects, `Novo Projeto ${Date.now()}`])
+
+    //console.log(projects)
+  }
+
   return (
     <Fragment>
-         <Header title="Home Page">
-           <ul>
-             <li>Homepage</li>
-             <li>Projects</li>
-           </ul>
-
-         </Header>
          <Header title="Projects">
            <ul>
-             <li>Homepage</li>
-             <li>Projects</li>
-             <li>Login</li>
+             {projects.map(p => <li key={p}>{p}</li>)}
            </ul>
+
+           <button type="button" onClick={handleAddProject} >Adicionar Projetos</button>
          </Header> 
     </Fragment>
   )
