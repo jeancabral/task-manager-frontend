4dd434c0e05f0fd741dde57c3e9d61c9b5dd9f37
Jean Cabral <jeancabral@gmail.com>
2020-04-15 11:19:40 -0300

    Componentização

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+
+import Header from './components/Header'
+
+function App(){
+  return (
+    <Fragment>
+         <Header/>
+         <Header/> 
+    </Fragment>
+  )
+}
+
+export default App;
